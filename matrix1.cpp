0637ec24c00f4ee8143946d750f80f1c46dd3f43
Gabriel Allba <rielgasy9@gmail.com>
2021-12-01 17:09:30 +0700

    add two dimensional array exercise

---
+++ matrix1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int n, m, a[100][100];
+	
+	scanf("%d %d", &n, &m);
+	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			scanf("%d", &a[i][j]);
+		}
+	}
+	
+	for(int i=n-1; i>=0; i--){
+		for(int j=m-1; j>=0; j--){
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
